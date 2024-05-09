f6fe9a0f6fffe2826f4f0d23d7b0e150d6f5680e
sumanth <sumanth@gstzen.in>
2024-05-09 08:31:32 +0530

    Initail frontend setup

--- setup.sh
+++ setup.sh
@@ -13,6 +13,16 @@ if [[ $PYTHON_VERSION == "Python 3.12.3" ]]; then
     source "${SCRIPT_DIR}"/.venv/bin/activate
     echo "Installing packages"
     uv pip install -r "${SCRIPT_DIR}"/requirements/dev.txt
+
+    echo "Installing frontend"
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+    nvm install 20
+    NODE_VERSION=$(node -v)
+    [[ $NODE_VERSION == "v20.13.0" ]]
+    corepack enable pnpm
+    echo "${NODE_VERSION} installed and pnpm enabled"
+    echo "Installing frontend packages now"
+    (cd "${SCRIPT_DIR}"/src/frontend && pnpm i)
 else
     echo "Python version incompatible"
 fi
